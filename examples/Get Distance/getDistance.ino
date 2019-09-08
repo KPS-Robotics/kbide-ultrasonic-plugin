554736048d69d9df54eb683e9ec3df9c25e7a34f
bavensky <apiruk_sang-ngenchai@hotmail.com>
2019-09-08 23:22:28 +0700

    update example code

--- examples/Get Distance/getDistance.ino
+++ examples/Get Distance/getDistance.ino
@@ -1,21 +1,23 @@
 #include <Arduino.h>
-#include <pins_arduino.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WiFiAP.h>
 #include <WebServer.h>
-#include <Wire.h>
+
 #include <Ultrasonic.h>
 
-ULTRASONIC ultrasonic;
+ULTRASONIC ultrasonic1;
+
+
 
 void setup()
 {
+  
   Serial.begin(115200);
-  ultrasonic.begin("4", "5");
+  ultrasonic1.begin(18, 19);
 }
 void loop()
 {
-  Serial.println(((uint8_t)ultrasonic.read_distance_cm()));
-  delay(200);
+  Serial.println(((uint8_t)ultrasonic1.read_distance_cm()));
+  delay(1000);
 }
