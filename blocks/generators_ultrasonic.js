79b8f7f3b581d7c7a100e7d9c7888e179905e8b5
Mikhail Akhsakov <85576471+Ax-47@users.noreply.github.com>
2024-05-03 12:07:49 +0700

    Update generators_ultrasonic.js

--- blocks/generators_ultrasonic.js
+++ blocks/generators_ultrasonic.js
@@ -1,9 +1,11 @@
-Blockly.JavaScript['ultrasonic_setup'] = function(block) {
-  var value_echo = block.getFieldValue('ECHO');
-  var value_trig = block.getFieldValue('TRIG');
-  var variable_instance = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('instance'), Blockly.Variables.NAME_TYPE);
-  var code =
-  `
+Blockly.JavaScript["ultrasonic_setup"] = function (block) {
+  var value_echo = block.getFieldValue("ECHO");
+  var value_trig = block.getFieldValue("TRIG");
+  var variable_instance = Blockly.JavaScript.variableDB_.getName(
+    block.getFieldValue("instance"),
+    Blockly.Variables.NAME_TYPE,
+  );
+  var code = `
   #EXTINC#include <Ultrasonic.h>#END
   #VARIABLEULTRASONIC ${variable_instance};#END
   ${variable_instance}.begin(${value_echo}, ${value_trig});
@@ -12,8 +14,11 @@ Blockly.JavaScript['ultrasonic_setup'] = function(block) {
   return code;
 };
 
-Blockly.JavaScript['ultrasonic_read_distance_cm'] = function(block) {
-  var variable_instance = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('instance'), Blockly.Variables.NAME_TYPE);
-  var code = `(uint8_t)${variable_instance}.read_distance_cm()`;
+Blockly.JavaScript["ultrasonic_read_distance_cm"] = function (block) {
+  var variable_instance = Blockly.JavaScript.variableDB_.getName(
+    block.getFieldValue("instance"),
+    Blockly.Variables.NAME_TYPE,
+  );
+  var code = `${variable_instance}.read_distance_cm()`;
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
